fbdb60f345c3938e29b8e44fc9cf94907d680480
I311761 <I311761@W-PF28NP5D>
2021-06-13 12:25:04 +0530

    second commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -3,6 +3,7 @@ package com.stacksimplify.restservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//testing commit
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
