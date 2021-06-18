bb51fbbbeeac7214a59ad3290a0f53ccfe96a8a9
I311761 <I311761@W-PF28NP5D>
2021-06-18 08:49:20 +0530

    JPA based rest service development completed

--- src/test/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplicationTests.java
+++ src/test/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplicationTests.java
@@ -1,13 +1,16 @@
 package com.stacksimplify.restservices;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class SpringbootBuildingblocksApplicationTests {
+public class SpringbootBuildingblocksApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
