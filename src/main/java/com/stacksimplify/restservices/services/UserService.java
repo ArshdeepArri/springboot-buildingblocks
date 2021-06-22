c4d67ac352aa89dd672cbe32cde21038fb1782e7
I311761 <arshdeeparri@gmail.com>
2021-06-22 19:56:55 +0530

    HATEOAS first commit

--- src/main/java/com/stacksimplify/restservices/services/UserService.java
+++ src/main/java/com/stacksimplify/restservices/services/UserService.java
@@ -50,7 +50,7 @@ public class UserService {
 			throw new UserNotFoundException("User not found in repository, provide the correct user id");
 		}
 		
-		user.setId(id);
+		user.setUserid(id);
 		return userRepository.save(user);
 	}
 	
